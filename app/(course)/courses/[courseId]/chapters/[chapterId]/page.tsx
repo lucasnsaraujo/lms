ebef335ce5aed5f6148e076e94cf55c9e32115de
Lucas Nasci <lucasnsaraujo0310@gmail.com>
2024-05-06 16:34:51 -0300

    creating attachments section

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -3,6 +3,10 @@ import { Banner } from "@/components/banner";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { VideoPlayer } from "./_components/video-player";
+import { CourseEnrollButton } from "./_components/course-enroll-button";
+import { Separator } from "@/components/ui/separator";
+import { Preview } from "@/components/preview";
+import { File } from "lucide-react";
 
 const ChapterIdPage = async ({
   params,
@@ -58,6 +62,42 @@ const ChapterIdPage = async ({
             completeOnEnd={completeOnEnd}
           />
         </div>
+      </div>
+      <div>
+        <div className="p-4 flex flex-col md:flex-row items-center justify-between">
+          <h2 className="text-2xl font-semibold mb-2">{chapter.title}</h2>
+          {purchase ? (
+            // Course progress Button
+            <></>
+          ) : (
+            <CourseEnrollButton
+              courseId={chapter.courseId}
+              price={course.price!}
+            />
+          )}
+        </div>
+        <Separator />
+        <div>
+          <Preview value={chapter.description!} />
+        </div>
+        {!!attachments.length && !!purchase && (
+          <>
+            <Separator />
+            <div className="p-4 space-y-2">
+              {attachments.map((attachment) => (
+                <a
+                  href={attachment.url}
+                  key={attachment.id}
+                  target="_blank"
+                  className="flex items-center p-3 w-full bg-sky-200 border text-sky-700 rounded-md hover:underline"
+                >
+                  <File />
+                  <p className="line-clamp-1">{attachment.name}</p>
+                </a>
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
